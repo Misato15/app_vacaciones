9c3d47e7fe6dda486329bee344c6b7d7bb55b555
Salvador Chinchilla <salvadorchinchilla03@gmail.com>
2022-12-05 23:19:45 -0600

    proyecto base de backend vacaciones

---
+++ src/models/empleado.model.ts
@@ -0,0 +1,40 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Empleado extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaIngreso: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  cargo_id: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  disponible: boolean;
+
+
+  constructor(data?: Partial<Empleado>) {
+    super(data);
+  }
+}
+
+export interface EmpleadoRelations {
+  // describe navigational properties here
+}
+
+export type EmpleadoWithRelations = Empleado & EmpleadoRelations;
